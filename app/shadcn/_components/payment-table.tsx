cf3e2c9ff1622ddc89f7b15e55ab02c38b683e1b
Nenad Brcic <neno@neno.ch>
2024-12-11 13:21:03 +0100

    Create payment table

--- app/shadcn/_components/payment-table.tsx
+++
@@ -1,8 +0,0 @@
-export function PaymentTable() {
-  return (
-    <div>
-      This is a Data Table Demo with Shadcn using @tanstack/react-table under
-      the hood
-    </div>
-  );
-}
--- app/shadcn/page.tsx
+++ app/shadcn/page.tsx
@@ -1,11 +1,15 @@
 import { getPayments } from "~/data-access/payments";
+import { PaymentsTable } from "./_components/payments-table";
 
 export default async function PageWithShadcn() {
+  const data = await getPayments();
   return (
     <div>
       <h1>Example using Shadcn</h1>
       <div className="grid grid-cols-8 gap-16">
-        <div className="col-span-5">TABLE</div>
+        <div className="col-span-5">
+          <PaymentsTable data={data} />
+        </div>
         <div className="col-span-3">FORM</div>
       </div>
     </div>
